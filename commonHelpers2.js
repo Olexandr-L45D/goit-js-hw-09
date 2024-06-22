b6d47f9bea2830df9f9680332682067d737f9c14
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-22 09:22:37 +0000

    Deploying to gh-pages from @ Olexandr-L45D/goit-js-hw-09@c9b52f937f1ac613bba9e29a9431c7b4370c294d 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */formData={email:"",message:""};
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const n=document.querySelector(".feedback-form");n.addEventListener("input",r);function r(t){const e=document.querySelector("input");e.textContent=t.currentTarget.value.trim(),t.currentTarget.value.trim()===""&&(e.textContent="Fill please all fields")}
 //# sourceMappingURL=commonHelpers2.js.map
