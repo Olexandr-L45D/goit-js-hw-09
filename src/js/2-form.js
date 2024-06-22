81e68e54c1bdf30050853d514329a57215d8757e
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-22 16:02:55 +0300

    upd 2 form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,6 +4,8 @@ const formData = {
 };
 
 const formTrackChanges = document.querySelector(".feedback-form");
+formTrackChanges.append(placeholder);
+placeholder.textContent = "Fill please all fields"
 formTrackChanges.addEventListener('input', handlerInput);
 
 function handlerInput(evt) {
@@ -13,4 +15,29 @@ function handlerInput(evt) {
      if (evt.currentTarget.value.trim() === "") {
         inputAray.textContent = "Fill please all fields"
      }
-    }
+    };
+    formTrackChanges.addEventListener('submit', handlerSubmit);
+
+    function handlerSubmit(event) {
+        event.preventDefault()
+        const {email, message} = event.currentTarget.elements;
+          const emailInputText = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+  };
+  if ( email.value.trim()  === "") {
+    alert("Fill please all fields") 
+};
+if ( message.value.trim()  === "") {
+    alert("Fill please all fields")  
+}
+  console.log(emailInputText);
+  form.reset()
+}
+    //     const textareaAray = document.querySelector('textarea')
+    //     textareaAray.textContent = event.currentTarget.value.trim();
+    
+    //  if (event.currentTarget.value.trim() === "") {
+    //     textareaAray.textContent = "Fill please all fields"
+    //  }
+    // }
