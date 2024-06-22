c9b52f937f1ac613bba9e29a9431c7b4370c294d
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-22 12:22:19 +0300

    summ racw 3

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,4 +1,16 @@
 //2form.js
-formData = {
+const formData = {
     email: "", message: ""
-}
+};
+
+const formTrackChanges = document.querySelector(".feedback-form");
+formTrackChanges.addEventListener('input', handlerInput);
+
+function handlerInput(evt) {
+      const inputAray = document.querySelector('input')
+      inputAray.textContent = evt.currentTarget.value.trim();
+    
+     if (evt.currentTarget.value.trim() === "") {
+        inputAray.textContent = "Fill please all fields"
+     }
+    }
