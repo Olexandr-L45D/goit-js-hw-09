7184def4274268bda1018209c9a705f04cdf34fc
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-22 19:44:41 +0300

    upd form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -17,27 +17,35 @@ const formTrackChanges = document.querySelector(".feedback-form");
     email: email.value.trim(),
     message: message.value.trim(),
   };
-  if ( email.value.trim()  === "") {
-    alert("Fill please all fields") 
-};
-if ( message.value.trim()  === "") {
-    alert("Fill please all fields")  
-}
+  if ( email.value.trim()  === "" || message.value.trim()  === "" ) {
+    alert("All form fields must be filled in") 
+    return;
+  }
+
+//   if ( email.value.trim()  === "") {
+//     alert("Fill please all fields") 
+// };
+// if ( message.value.trim()  === "") {
+//     alert("Fill please all fields")  
+// }
   console.log(emailInputText);
   formData.push(emailInputText).join("")
   console.log(formData);
-  formTrackChanges.addEventListener('input', handlerInput);
 
-function handlerInput(evt) {
-      const inputAray = document.querySelector('input')
-      inputAray.textContent = evt.currentTarget.value.trim();
+//   formTrackChanges.addEventListener('input', handlerInput);
+
+// function handlerInput(evt) {
+//       const inputAray = document.querySelector('input')
+//       inputAray.textContent = evt.currentTarget.value.trim();
     
-     if (evt.currentTarget.value.trim() === "") {
-        inputAray.textContent = "Fill please all fields"
-     }
-    };
+//      if (evt.currentTarget.value.trim() === "") {
+//         inputAray.textContent = "Fill please all fields"
+//      }
+//     };
 //   form.reset()
 }
+
+
     //     const textareaAray = document.querySelector('textarea')
     //     textareaAray.textContent = event.currentTarget.value.trim();
     
