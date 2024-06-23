0ca491ccdb7eedc63bcb60d711b659f6f558d615
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-23 21:39:22 +0300

    14

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -63,21 +63,29 @@ const textareaTaxt = formTrackChanges.elements.message;
 const inputTaxt = formTrackChanges.elements.message;
 const localStorageKey = "feedback-form-state";
 
+
 inputTaxt.value = localStorage.getItem(localStorageKey);
 textareaTaxt.value = localStorage.getItem(localStorageKey);
 
 formTrackChanges.addEventListener("input", (evt) => {
   localStorage.setItem(localStorageKey, evt.target.value);
- 
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
 });
 
 formTrackChanges.addEventListener("submit", (evt) => {
   evt.preventDefault();
 	console.log(evt.target.elements.message.value);
   localStorage.removeItem(localStorageKey);
-  if ( email.value.trim()  === "" || message.value.trim()  === "" ) {
-        alert("All form fields must be filled in") 
-         return; };
+  const formDataValue = formData;
+        formDataValue = even.currentTarget.elements; 
+        const valueContent = {
+          email: email.value.trim(),
+          message: message.value.trim()} ;
+    if ( email.value.trim()  === "" || message.value.trim()  === "") {
+      alert("All form fields must be filled in") 
+      return;
+    }
+    console.log(valueContent);
   formTrackChanges.reset();
 });
 
