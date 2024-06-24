7dc9263edff0fdfb835c423aa80fad4b9fd98e4d
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-24 07:21:52 +0300

    2 mon

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -12,6 +12,8 @@ const localStorageKey = "feedback-form-state";
 inputTaxt.value = localStorage.getItem(localStorageKey);
 textareaTaxt.value = localStorage.getItem(localStorageKey);
 
+// console.log(JSON.parse(valueForm));
+
 formTrackChanges.addEventListener("input", (evt) => {
   localStorage.setItem(localStorageKey, evt.target.value);
   localStorage.setItem(localStorageKey, JSON.stringify(formData));
@@ -22,9 +24,15 @@ formTrackChanges.addEventListener("submit", (evt) => {
 	console.log(evt.target.elements.message.value);
   localStorage.removeItem(localStorageKey);
  
-  formData = evt.currentTarget.elements; 
-  formData.email = email.value.trim(),
-  formData.message = message.value.trim() ;
+  // formData = evt.currentTarget.elements; 
+  // formData.email = email.value.trim(),
+  // formData.message = message.value.trim() ;
+
+  const { email, message} = evt.currentTarget.elements; 
+  const valueContent = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+  };
     if ( email.value.trim()  === "" || message.value.trim()  === "") {
       alert("All form fields must be filled in") 
       return;
@@ -33,3 +41,22 @@ formTrackChanges.addEventListener("submit", (evt) => {
   formTrackChanges.reset();
 });
 
+
+
+// const dog = {
+//   name: "Mango",
+//   age: 3,
+//   isGoodBoy: true,
+//   bark() {
+//     console.log("Woof!");
+//   },
+// };
+
+// const json = JSON.stringify(dog);
+// console.log(json); // 
+
+// const json = '{"name":"Mango","age":3,"isGoodBoy":true}';
+
+// const dog = JSON.parse(json);
+// console.log(dog); // {name: "Mango", age: 3, isGoodBoy: true}
+// console.log(dog.name); // "Mango"
