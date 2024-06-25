7b1e04bec3cf4150902a7dd998eb09d4ee7ab080
Olexandr-L45D <160649564+Olexandr-L45D@users.noreply.github.com>
2024-06-25 12:46:21 +0300

    upd f 2 - 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -14,9 +14,10 @@ textareaTaxts.value = localStorage.getItem(localStorageKey);
 formTrackChanges.addEventListener("input", (evt) => {
   localStorage.setItem(localStorageKey, evt.target.value);
   let inputTaxts = document.querySelector('.input')
-// inputTaxts.textContent = evt.currentTarget.email.value;
+inputTaxts.textContent = evt.currentTarget.email.value;
 let textareaTaxts = document.querySelector('textarea')
-// textareaTaxts.textContent = evt.currentTarget.message.value;
+textareaTaxts.textContent = evt.currentTarget.message.value;
+// formData = {inputTaxts, textareaTaxts };
 formData = `${email.value} ${message.value} `;
 const valueForm = JSON.stringify(formData);
 const valueFormParse = JSON.parse(valueForm);
